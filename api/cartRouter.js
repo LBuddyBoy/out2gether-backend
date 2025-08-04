9c201aaedf21e2bb97479b2017a2628f8b283d98
Ethan Toups <ethantoups05@gmail.com>
2025-08-04 18:59:34 -0500

    Updated logic for adding to cart

--- api/cartRouter.js
+++ api/cartRouter.js
@@ -20,8 +20,20 @@ router
 
     res.status(200).json(cart_items);
   })
-  .post(requireBody(["post_id", "quantity"]), async (req, res) => {
-    const cart_item = await createCartItem({
+  .post(requireBody(["post_id"]), async (req, res) => {
+    const { post_id } = req.body;
+
+    let cart_item = await getCartItem(req.user.id, post_id);
+
+    if (cart_item) {
+      cart_item = await updateCartItem(req.user.id, post_id, {
+        quantity: cart_item.quantity + 1,
+      });
+
+      return res.status(200).json(cart_item);
+    }
+
+    cart_item = await createCartItem({
       owner_id: req.user.id,
       ...req.body,
     });
@@ -32,6 +44,9 @@ router
 router.param("postId", async (req, res, next, postId) => {
   const cart_item = await getCartItem(req.user.id, postId);
 
+  if (!cart_item)
+    return res.status(404).send("Couldn't find a cart item with that post id.");
+
   req.cart_item = cart_item;
   next();
 });
@@ -39,14 +54,9 @@ router.param("postId", async (req, res, next, postId) => {
 router
   .route("/:postId")
   .get(async (req, res) => {
-    res.status(200).json(req.cart_item || false);
+    res.status(200).json(req.cart_item);
   })
   .put(async (req, res) => {
-    if (!req.cart_item)
-      return res
-        .status(404)
-        .send("Couldn't find a cart item with that post id.");
-
     const update = await updateCartItem(
       req.cart_item.owner_id,
       req.cart_item.post_id,
@@ -60,11 +70,6 @@ router
     res.status(200).json(update);
   })
   .delete(async (req, res) => {
-    if (!req.cart_item)
-      return res
-        .status(404)
-        .send("Couldn't find a cart item with that post id.");
-
     await deleteCartItem(req.cart_item.owner_id, req.cart_item.post_id);
     res.status(204).send("Successfully deleted!");
   });
