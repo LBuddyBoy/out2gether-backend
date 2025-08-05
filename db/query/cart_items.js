fd9deadb4c64a842b3acbb8694c986239643d644
Knox Wong <wongknox@gmail.com>
2025-08-05 16:30:57 -0500

    Updated createCartItem function with ON CONFLICT to update cart quantity if item already exists in cart

--- db/query/cart_items.js
+++ db/query/cart_items.js
@@ -6,6 +6,8 @@ export async function createCartItem({ post_id, owner_id, quantity }) {
   const SQL = `
   INSERT INTO cart_items(post_id, owner_id, quantity)
   VALUES($1, $2, $3)
+  ON CONFLICT (post_id, owner_id) DO UPDATE
+  SET quantity = cart_items.quantity + $3
   RETURNING *
   `;
 
