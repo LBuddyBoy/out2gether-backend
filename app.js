3fa388a2d560566495a2721d06b71089c46cd98e
Ethan Toups <ethantoups05@gmail.com>
2025-08-11 20:44:40 -0500

    Fixed cors 2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true, // only if you send cookies/Authorization
+    credentials: true,
   })
 );
 
