0d734f3de4be0a98ee8208aeae694a579673a978
LBuddyBoy <pokemonsimple@gmail.com>
2026-07-25 14:56:23 -0500

    updated

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import app from "#app";
 import db from "#db/client";
 
+const HOST = process.env.HOST ?? "127.0.0.1";
 const PORT = process.env.PORT ?? 3000;
 
 await db.connect();
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+app.listen(PORT, HOST, () => {
+  console.log(`Listening on ${HOST}:${PORT}...`);
 });
